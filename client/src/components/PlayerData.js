9f2104133d02707279487e8a2daa3382e59361d9
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-11-08 21:40:56 -0500

    Added PlayerData Component

---
+++ client/src/components/PlayerData.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+import PlayerCard from './PlayerCard';
+
+class PlayerData extends React.Component {
+  constructor() {
+    super();
+    this._isMounted = false;
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this.setState({isMounted: true}, () => {
+      axios.get(`http://localhost:5000/api/players`)
+        .then(res => {
+          // console.log(res);
+          if(this._isMounted) {
+            this.setState({
+              players: res.data
+            });
+          }
+        })
+        .catch(err => console.log(err))
+    });
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.players.map(player => {
+          return (
+            <PlayerCard
+              key={player.id}
+              name={player.name}
+              country={player.country}
+              searches={player.searches}
+            />  
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default PlayerData;
